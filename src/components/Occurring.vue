c686f989ce3c2d845165fcf116397586e4cdd061
github nitf <2tf2003@gmail.com>
2019-09-25 16:44:51 +0900

    わからん...

--- src/components/Occurring.vue
+++ src/components/Occurring.vue
@@ -3,9 +3,20 @@
         <ul>
             <li class="box_small" id="left">
                 <p>離岸流の発生</p>
+                <div class="image">
+                    <h3>離岸流が発生しています</h3>
+                    発生地点で警告音が鳴っています<br>
+                    十分に警戒してください
+                </div>
             </li>
             <li class="box_small" id="right">
                 <p>離岸流の発生場所</p>
+                <div class="occur_place">
+                    <ul type="disc" class="ul">
+                        <li><span>4番ポール</span></li>
+                        <li><span>5番ポール</span></li>
+                    </ul>
+                </div>
             </li>
         </ul>
 
@@ -15,12 +26,43 @@
 </template>
 
 <script>
+    import AssetsImage from "@/assets/warning.png";
     export default {
-        name: "Occurring"
+        name: "Occurring",
+        data() {
+            return {
+                assetsImage: AssetsImage,
+                assetsImage_NG: "../assets/warning.png",
+                staticImage: "/warning.png"
+            };
+        }
     }
 </script>
 
 <style scoped>
+
+    .ul{
+        color: #4cbbb4;
+    }
+
+    span{
+        color: gray;
+    }
+
+    h3{
+        color: #ef8468;
+    }
+    .image{
+        text-align: left;
+        color: darkgray;
+    }
+
+    .occur_place{
+        font-size: 30px;
+        margin-top: 50px;
+        text-align: left;
+        color: gray;
+    }
     .occurring{
         position: relative;
         height: 250px;
@@ -40,7 +82,6 @@
         padding: 0;
         text-align: center;
         top: -25px;
-        text-align: center;
         font-weight: normal;
         font-size: 17px;
         color: #4CBBB4;
